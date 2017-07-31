671315e8c03ba7692b1a20641bbb3cd0f935adc2
Dominik Zogg <dominik.zogg@gmail.com>
2017-07-31 13:42:48 +0200

    add type hint

--- tests/Resources/ModelMapping.php
+++ tests/Resources/ModelMapping.php
@@ -49,13 +49,13 @@ final class ModelMapping implements ObjectMappingInterface
     public function getLinkMappings(): array
     {
         return [
-            new LinkMapping('name:read', new CallbackLinkSerializer(function (Request $request, $object) {
+            new LinkMapping('name:read', new CallbackLinkSerializer(function (Request $request, Model $object) {
                 return new Link('http://test.com/models/'.$object->getId(), Link::METHOD_GET);
             })),
-            new LinkMapping('name:update', new CallbackLinkSerializer(function (Request $request, $object) {
+            new LinkMapping('name:update', new CallbackLinkSerializer(function (Request $request, Model$object) {
                 return new Link('http://test.com/models/'.$object->getId(), Link::METHOD_PUT);
             })),
-            new LinkMapping('name:delete', new CallbackLinkSerializer(function (Request $request, $object) {
+            new LinkMapping('name:delete', new CallbackLinkSerializer(function (Request $request, Model $object) {
                 return new Link('http://test.com/models/'.$object->getId(), Link::METHOD_DELETE);
             })),
         ];
