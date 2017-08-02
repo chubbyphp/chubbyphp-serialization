bb39f0bf71e62914f1a1255a5efb268c8e4e4f15
Dominik Zogg <dominik.zogg@gmail.com>
2017-08-02 22:15:35 +0200

    add xml formatter

---
+++ src/Formatter/XmlFormatter.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Serialization\Formatter;
+
+use Doctrine\Common\Inflector\Inflector;
+
+final class XmlFormatter implements FormatterInterface
+{
+    /**
+     * @var bool
+     */
+    private $formatOutput;
+
+    /**
+     * @param bool $formatOutput
+     */
+    public function __construct(bool $formatOutput = false)
+    {
+        $this->formatOutput = $formatOutput;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return string
+     */
+    public function format(array $data): string
+    {
+        $document = new \DOMDocument('1.0', 'UTF-8');
+        $document->formatOutput = $this->formatOutput;
+
+        $this->dataToNodes($document, $document, $data);
+
+        return $document->saveXML();
+    }
+
+    /**
+     * @param \DOMDocument $document
+     * @param \DOMNode     $listNode
+     * @param array        $data
+     */
+    private function dataToNodes(\DOMDocument $document, \DOMNode $listNode, array $data)
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $childNode = $document->createElement(is_int($key) ? Inflector::singularize($listNode->nodeName) : $key);
+                $this->dataToNodes($document, $childNode, $value);
+            } else {
+                $childNode = $document->createElement($key, $value);
+            }
+
+            $listNode->appendChild($childNode);
+        }
+    }
+}
--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Chubbyphp\Tests\Serialization;
 
 use Chubbyphp\Serialization\Formatter\JsonFormatter;
+use Chubbyphp\Serialization\Formatter\XmlFormatter;
 use Chubbyphp\Serialization\Formatter\YamlFormatter;
 use Chubbyphp\Serialization\Registry\ObjectMappingRegistry;
 use Chubbyphp\Serialization\Serializer;
@@ -140,6 +141,47 @@ _links:
 EOD;
 
         self::assertEquals($expectedYaml, $yaml);
+
+        $xmlFormatter = new XmlFormatter(true);
+        $xml = $xmlFormatter->format($data);
+
+        $expectedXml = <<<EOD
+<?xml version="1.0" encoding="UTF-8"?>
+<name>name1</name>
+<_embedded>
+  <embeddedModel>
+    <name>embedded1</name>
+  </embeddedModel>
+  <embeddedModels>
+    <embeddedModel>
+      <name>embedded2</name>
+    </embeddedModel>
+    <embeddedModel>
+      <name>embedded3</name>
+    </embeddedModel>
+    <embeddedModel>
+      <name>embedded4</name>
+    </embeddedModel>
+  </embeddedModels>
+</_embedded>
+<_links>
+  <name:read>
+    <href>http://test.com/models/id1</href>
+    <method>GET</method>
+  </name:read>
+  <name:update>
+    <href>http://test.com/models/id1</href>
+    <method>PUT</method>
+  </name:update>
+  <name:delete>
+    <href>http://test.com/models/id1</href>
+    <method>DELETE</method>
+  </name:delete>
+</_links>
+
+EOD;
+
+        self::assertEquals($expectedXml, $xml);
     }
 
     /**
