4d3943d5dced3056f746e0b98c237f07d3e5aae9
Dominik Zogg <dominik.zogg@gmail.com>
2017-08-03 21:42:47 +0200

    add link test

---
+++ tests/Link/LinkTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Serialization\Link;
+
+use Chubbyphp\Serialization\Link\Link;
+
+/**
+ * @covers \Chubbyphp\Serialization\Link\Link
+ */
+class LinkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetValue()
+    {
+        $link = new Link('http://test.com/models/id1', Link::METHOD_GET, ['key' => 'value']);
+
+        self::assertEquals([
+            'href' => 'http://test.com/models/id1',
+            'method' => 'GET',
+            'attributes' => ['key' => 'value']
+        ], $link->jsonSerialize());
+    }
+}
