edadc9d0058542f03038e9b4c0de80e293b7adc6
Dominik Zogg <dominik.zogg@gmail.com>
2017-10-30 21:35:45 +0100

    add embed normalizer field test

---
+++ tests/Normalizer/Relation/EmbedManyFieldNormalizerTest.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Serialization\Normalizer\Relation;
+
+use Chubbyphp\Serialization\Accessor\AccessorInterface;
+use Chubbyphp\Serialization\Normalizer\NormalizerContextInterface;
+use Chubbyphp\Serialization\Normalizer\Relation\EmbedManyFieldNormalizer;
+use Chubbyphp\Serialization\Normalizer\NormalizerInterface;
+use Chubbyphp\Serialization\SerializerLogicException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Chubbyphp\Serialization\Normalizer\Relation\EmbedManyFieldNormalizer
+ */
+class EmbedManyFieldNormalizerTest extends TestCase
+{
+    public function testNormalizeMissingNormalizer()
+    {
+        self::expectException(SerializerLogicException::class);
+        self::expectExceptionMessage('There is no normalizer at path: "children"');
+
+        $fieldNormalizer = new EmbedManyFieldNormalizer($this->getAccessor());
+
+        $fieldNormalizer->normalizeField('children', new \stdClass(), $this->getNormalizerContext());
+    }
+
+    public function testNormalize()
+    {
+        $parent = $this->getParent();
+        $parent->setChildren([
+            $this->getChild()->setName('name1'),
+            $this->getChild()->setName('name2'),
+        ]);
+
+        $fieldNormalizer = new EmbedManyFieldNormalizer($this->getAccessor());
+
+        self::assertSame(
+            [['name' => 'name1'], ['name' => 'name2']],
+            $fieldNormalizer->normalizeField(
+                'children',
+                $parent,
+                $this->getNormalizerContext(),
+                $this->getNormalizer()
+            )
+        );
+    }
+
+    public function testNormalizeEmpty()
+    {
+        $parent = $this->getParent();
+        $parent->setChildren([]);
+
+        $fieldNormalizer = new EmbedManyFieldNormalizer($this->getAccessor());
+
+        self::assertSame(
+            [],
+            $fieldNormalizer->normalizeField(
+                'children',
+                $parent,
+                $this->getNormalizerContext(),
+                $this->getNormalizer()
+            )
+        );
+    }
+
+    public function testNormalizeNull()
+    {
+        $parent = $this->getParent();
+
+        $fieldNormalizer = new EmbedManyFieldNormalizer($this->getAccessor());
+
+        self::assertNull(
+            $fieldNormalizer->normalizeField(
+                'children',
+                $parent,
+                $this->getNormalizerContext(),
+                $this->getNormalizer()
+            )
+        );
+    }
+
+    /**
+     * @return object
+     */
+    private function getParent()
+    {
+        return new class() {
+            /**
+             * @var array|null
+             */
+            private $children;
+
+            /**
+             * @return array
+             */
+            public function getChildren()
+            {
+                return $this->children;
+            }
+
+            /**
+             * @param array|null $children
+             *
+             * @return self
+             */
+            public function setChildren(array $children = null): self
+            {
+                $this->children = $children;
+
+                return $this;
+            }
+        };
+    }
+
+    /**
+     * @return object
+     */
+    private function getChild()
+    {
+        return new class() {
+            /**
+             * @var string
+             */
+            private $name;
+
+            /**
+             * @return string
+             */
+            public function getName(): string
+            {
+                return $this->name;
+            }
+
+            /**
+             * @param string $name
+             *
+             * @return self
+             */
+            public function setName(string $name): self
+            {
+                $this->name = $name;
+
+                return $this;
+            }
+        };
+    }
+
+    /**
+     * @return AccessorInterface
+     */
+    private function getAccessor(): AccessorInterface
+    {
+        /** @var AccessorInterface|\PHPUnit_Framework_MockObject_MockObject $accessor */
+        $accessor = $this->getMockBuilder(AccessorInterface::class)->getMockForAbstractClass();
+
+        $accessor->expects(self::any())->method('getValue')->willReturnCallback(function ($object) {
+            return $object->getChildren();
+        });
+
+        return $accessor;
+    }
+
+    /**
+     * @return NormalizerContextInterface
+     */
+    private function getNormalizerContext(): NormalizerContextInterface
+    {
+        /** @var NormalizerContextInterface|\PHPUnit_Framework_MockObject_MockObject $context */
+        $context = $this->getMockBuilder(NormalizerContextInterface::class)->getMockForAbstractClass();
+
+        return $context;
+    }
+
+    /**
+     * @return NormalizerInterface
+     */
+    private function getNormalizer(): NormalizerInterface
+    {
+        /** @var NormalizerInterface|\PHPUnit_Framework_MockObject_MockObject $normalizer */
+        $normalizer = $this->getMockBuilder(NormalizerInterface::class)->getMockForAbstractClass();
+
+        $normalizer->expects(self::any())->method('normalize')->willReturnCallback(
+            function ($object, NormalizerContextInterface $context = null, string $path = '') {
+                return ['name' => $object->getName()];
+            }
+        );
+
+        return $normalizer;
+    }
+}
---
+++ tests/Normalizer/Relation/EmbedOneFieldNormalizerTest.php
@@ -0,0 +1,172 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Serialization\Normalizer;
+
+use Chubbyphp\Serialization\Normalizer\NormalizerContextInterface;
+use Chubbyphp\Serialization\Normalizer\NormalizerInterface;
+use Chubbyphp\Serialization\Normalizer\Relation\EmbedOneFieldNormalizer;
+use Chubbyphp\Serialization\Accessor\AccessorInterface;
+use Chubbyphp\Serialization\SerializerLogicException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Chubbyphp\Serialization\Normalizer\Relation\EmbedOneFieldNormalizer
+ */
+class EmbedOneFieldNormalizerTest extends TestCase
+{
+    public function testNormalizeFieldWithMissingNormalizer()
+    {
+        self::expectException(SerializerLogicException::class);
+        self::expectExceptionMessage('There is no normalizer at path: "relation"');
+
+        $object = $this->getObject();
+
+        $fieldNormalizer = new EmbedOneFieldNormalizer($this->getAccessor());
+
+        $fieldNormalizer->normalizeField('relation', $object, $this->getNormalizerContext());
+    }
+
+    public function testNormalizeFieldWithNull()
+    {
+        $object = $this->getObject();
+
+        $fieldNormalizer = new EmbedOneFieldNormalizer($this->getAccessor());
+
+        $data = $fieldNormalizer->normalizeField(
+            'relation',
+            $object,
+            $this->getNormalizerContext(),
+            $this->getNormalizer()
+        );
+
+        self::assertSame(null, $data);
+    }
+
+    public function testNormalizeFieldWithObject()
+    {
+        $object = $this->getObject();
+        $object->setReference($this->getRelation()->setName('php'));
+
+        $fieldNormalizer = new EmbedOneFieldNormalizer($this->getAccessor());
+
+        $data = $fieldNormalizer->normalizeField(
+            'relation',
+            $object,
+            $this->getNormalizerContext(),
+            $this->getNormalizer()
+        );
+
+        self::assertSame(['name' => 'php'], $data);
+    }
+
+    /**
+     * @return AccessorInterface
+     */
+    private function getAccessor(): AccessorInterface
+    {
+        /** @var AccessorInterface|\PHPUnit_Framework_MockObject_MockObject $accessor */
+        $accessor = $this->getMockBuilder(AccessorInterface::class)->getMockForAbstractClass();
+
+        $accessor->expects(self::any())->method('getValue')->willReturnCallback(function ($object) {
+            return $object->getRelation();
+        });
+
+        return $accessor;
+    }
+
+    /**
+     * @return NormalizerInterface
+     */
+    private function getNormalizer(): NormalizerInterface
+    {
+        /** @var NormalizerInterface|\PHPUnit_Framework_MockObject_MockObject $normalizer */
+        $normalizer = $this->getMockBuilder(NormalizerInterface::class)->getMockForAbstractClass();
+
+        $normalizer->expects(self::any())->method('normalize')->willReturnCallback(
+            function ($object, NormalizerContextInterface $context = null, string $path = '') {
+                return ['name' => $object->getName()];
+            }
+        );
+
+        return $normalizer;
+    }
+
+    /**
+     * @return NormalizerContextInterface
+     */
+    private function getNormalizerContext(): NormalizerContextInterface
+    {
+        /** @var NormalizerContextInterface|\PHPUnit_Framework_MockObject_MockObject $context */
+        $context = $this->getMockBuilder(NormalizerContextInterface::class)->getMockForAbstractClass();
+
+        return $context;
+    }
+
+    /**
+     * @return object
+     */
+    private function getObject()
+    {
+        return new class() {
+            /**
+             * @var object
+             */
+            private $relation;
+
+            /**
+             * @return object
+             */
+            public function getRelation()
+            {
+                return $this->relation;
+            }
+
+            /**
+             * @param object $relation
+             *
+             * @return self
+             */
+            public function setReference($relation): self
+            {
+                $this->relation = $relation;
+
+                return $this;
+            }
+        };
+    }
+
+    /**
+     * @return object
+     */
+    private function getRelation()
+    {
+        return new class() {
+            /**
+             * @var string
+             */
+            private $name;
+
+            /**
+             * @return string
+             */
+            public function getName(): string
+            {
+                return $this->name;
+            }
+
+            /**
+             * @param string $name
+             *
+             * @return self
+             */
+            public function setName(string $name): self
+            {
+                $this->name = $name;
+
+                return $this;
+            }
+        };
+    }
+}
