476f473943e2150d6f69b9f9d34ce7ce6a55248f
healerz <m.haller.ch@gmail.com>
2019-04-24 17:25:26 +0200

    feat(JMP-592): add docblock for policy interface method

--- src/Policy/PolicyInterface.php
+++ src/Policy/PolicyInterface.php
@@ -8,5 +8,11 @@ use Chubbyphp\Serialization\Normalizer\NormalizerContextInterface;
 
 interface PolicyInterface
 {
+    /**
+     * @param NormalizerContextInterface $context
+     * @param object                     $object
+     *
+     * @return bool
+     */
     public function isCompliant(NormalizerContextInterface $context, $object): bool;
 }
