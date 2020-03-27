4310345e0a2baa54c3ade4e75008ac5fe808dfa6
Marko Ilic <marko.ilic@jobcloud.ch>
2020-03-27 17:14:13 +0100

    feat(JMP-1265): remove isCompliantIcludingPath from CallbackPolicy

--- src/Policy/CallbackPolicy.php
+++ src/Policy/CallbackPolicy.php
@@ -6,6 +6,9 @@ namespace Chubbyphp\Serialization\Policy;
 
 use Chubbyphp\Serialization\Normalizer\NormalizerContextInterface;
 
+/**
+ * @deprecated Use CallbackPolicyIncludingPath instead
+ */
 final class CallbackPolicy implements PolicyInterface
 {
     /**
@@ -18,18 +21,10 @@ final class CallbackPolicy implements PolicyInterface
         $this->callback = $callback;
     }
 
-    /**
-     * @deprecated
-     */
     public function isCompliant(NormalizerContextInterface $context, object $object): bool
     {
         @trigger_error('Use "isCompliantIncludingPath()" instead of "isCompliant()"', E_USER_DEPRECATED);
 
         return ($this->callback)($context, $object);
-    }
-
-    public function isCompliantIncludingPath(object $object, NormalizerContextInterface $context, string $path): bool
-    {
-        return ($this->callback)($context, $object, $path);
     }
 }
