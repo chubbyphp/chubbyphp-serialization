cfaa050e869bef5dc18343a9437553b39067138a
Dominik Zogg <dominik.zogg@hotmail.com>
2022-09-14 19:18:06 +0200

    SerializerFactory should use Chubbyphp\DecodeEncode\Encoder\EncoderInterface

--- src/ServiceFactory/SerializerFactory.php
+++ src/ServiceFactory/SerializerFactory.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Serialization\ServiceFactory;
 
+use Chubbyphp\DecodeEncode\Encoder\EncoderInterface;
+use Chubbyphp\DecodeEncode\ServiceFactory\EncoderFactory;
 use Chubbyphp\Laminas\Config\Factory\AbstractFactory;
-use Chubbyphp\Serialization\Encoder\EncoderInterface;
+use Chubbyphp\Serialization\Encoder\EncoderInterface as OldEncoderInterface;
 use Chubbyphp\Serialization\Normalizer\NormalizerInterface;
 use Chubbyphp\Serialization\Serializer;
 use Chubbyphp\Serialization\SerializerInterface;
@@ -18,8 +20,22 @@ final class SerializerFactory extends AbstractFactory
         /** @var NormalizerInterface $normalizer */
         $normalizer = $this->resolveDependency($container, NormalizerInterface::class, NormalizerFactory::class);
 
-        /** @var EncoderInterface $encoder */
-        $encoder = $this->resolveDependency($container, EncoderInterface::class, EncoderFactory::class);
+        if ($container->has(OldEncoderInterface::class.$this->name)) {
+            @trigger_error(
+                sprintf(
+                    '%s use %s',
+                    OldEncoderInterface::class,
+                    EncoderInterface::class
+                ),
+                E_USER_DEPRECATED
+            );
+
+            /** @var OldEncoderInterface $encoder */
+            $encoder = $container->get(OldEncoderInterface::class.$this->name);
+        } else {
+            /** @var EncoderInterface $encoder */
+            $encoder = $this->resolveDependency($container, EncoderInterface::class, EncoderFactory::class);
+        }
 
         return new Serializer($normalizer, $encoder);
     }
--- tests/Unit/ServiceFactory/SerializerFactoryTest.php
+++ tests/Unit/ServiceFactory/SerializerFactoryTest.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Tests\Serialization\Unit\ServiceFactory;
 
+use Chubbyphp\DecodeEncode\Encoder\EncoderInterface;
 use Chubbyphp\Mock\Call;
 use Chubbyphp\Mock\MockByCallsTrait;
-use Chubbyphp\Serialization\Encoder\EncoderInterface;
+use Chubbyphp\Serialization\Encoder\EncoderInterface as OldEncoderInterface;
 use Chubbyphp\Serialization\Normalizer\NormalizerInterface;
 use Chubbyphp\Serialization\SerializerInterface;
 use Chubbyphp\Serialization\ServiceFactory\SerializerFactory;
@@ -22,6 +23,78 @@ final class SerializerFactoryTest extends TestCase
 {
     use MockByCallsTrait;
 
+    public function testInvokeWithDeprecatedEncoderInterface(): void
+    {
+        /** @var NormalizerInterface $normalizer */
+        $normalizer = $this->getMockByCalls(NormalizerInterface::class);
+
+        /** @var EncoderInterface $encoder */
+        $encoder = $this->getMockByCalls(OldEncoderInterface::class);
+
+        /** @var ContainerInterface $container */
+        $container = $this->getMockByCalls(ContainerInterface::class, [
+            Call::create('has')->with(NormalizerInterface::class)->willReturn(true),
+            Call::create('get')->with(NormalizerInterface::class)->willReturn($normalizer),
+            Call::create('has')->with(OldEncoderInterface::class)->willReturn(true),
+            Call::create('get')->with(OldEncoderInterface::class)->willReturn($encoder),
+        ]);
+
+        $factory = new SerializerFactory();
+
+        error_clear_last();
+
+        $service = $factory($container);
+
+        $error = error_get_last();
+
+        self::assertNotNull($error);
+
+        self::assertSame(E_USER_DEPRECATED, $error['type']);
+        self::assertSame(sprintf(
+            '%s use %s',
+            OldEncoderInterface::class,
+            EncoderInterface::class
+        ), $error['message']);
+
+        self::assertInstanceOf(SerializerInterface::class, $service);
+    }
+
+    public function testCallStaticWithDeprecatedEncoderInterface(): void
+    {
+        /** @var NormalizerInterface $normalizer */
+        $normalizer = $this->getMockByCalls(NormalizerInterface::class);
+
+        /** @var OldEncoderInterface $encoder */
+        $encoder = $this->getMockByCalls(OldEncoderInterface::class);
+
+        /** @var ContainerInterface $container */
+        $container = $this->getMockByCalls(ContainerInterface::class, [
+            Call::create('has')->with(NormalizerInterface::class.'default')->willReturn(true),
+            Call::create('get')->with(NormalizerInterface::class.'default')->willReturn($normalizer),
+            Call::create('has')->with(OldEncoderInterface::class.'default')->willReturn(true),
+            Call::create('get')->with(OldEncoderInterface::class.'default')->willReturn($encoder),
+        ]);
+
+        $factory = [SerializerFactory::class, 'default'];
+
+        error_clear_last();
+
+        $service = $factory($container);
+
+        $error = error_get_last();
+
+        self::assertNotNull($error);
+
+        self::assertSame(E_USER_DEPRECATED, $error['type']);
+        self::assertSame(sprintf(
+            '%s use %s',
+            OldEncoderInterface::class,
+            EncoderInterface::class
+        ), $error['message']);
+
+        self::assertInstanceOf(SerializerInterface::class, $service);
+    }
+
     public function testInvoke(): void
     {
         /** @var NormalizerInterface $normalizer */
@@ -34,6 +107,7 @@ final class SerializerFactoryTest extends TestCase
         $container = $this->getMockByCalls(ContainerInterface::class, [
             Call::create('has')->with(NormalizerInterface::class)->willReturn(true),
             Call::create('get')->with(NormalizerInterface::class)->willReturn($normalizer),
+            Call::create('has')->with(OldEncoderInterface::class)->willReturn(false),
             Call::create('has')->with(EncoderInterface::class)->willReturn(true),
             Call::create('get')->with(EncoderInterface::class)->willReturn($encoder),
         ]);
@@ -57,6 +131,7 @@ final class SerializerFactoryTest extends TestCase
         $container = $this->getMockByCalls(ContainerInterface::class, [
             Call::create('has')->with(NormalizerInterface::class.'default')->willReturn(true),
             Call::create('get')->with(NormalizerInterface::class.'default')->willReturn($normalizer),
+            Call::create('has')->with(OldEncoderInterface::class.'default')->willReturn(false),
             Call::create('has')->with(EncoderInterface::class.'default')->willReturn(true),
             Call::create('get')->with(EncoderInterface::class.'default')->willReturn($encoder),
         ]);
